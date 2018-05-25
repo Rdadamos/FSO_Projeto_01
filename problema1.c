c8c1b4194828901de024497b984c893064afc49f
Rdadamos <rodrigodadamos@gmail.com>
2018-05-25 20:02:57 -0300

    Add check subgrid function

--- problema1.c
+++ problema1.c
@@ -4,16 +4,18 @@
 #define SIZE 9
 
 FILE *file;
-int result[SIZE];
+int result[SIZE*3];
 
+int checkLine(int lineNum);
 int checkColumn(int columnNum);
+int checkSubgrid(int subgridNum);
 int checkSudoku(int array[SIZE]);
 
 int main(int argc, char const *argv[]) {
 
   if(argc != 2)
   {
-    fprintf(stderr, "usage: problema1.out <file name>\n");
+    fprintf(stderr, "Usage: problema1.out <file name>\n");
     return -1;
   }
   if (file = fopen(argv[1], "r"))
@@ -21,30 +23,51 @@ int main(int argc, char const *argv[]) {
     int i;
     for (i = 0; i < SIZE; i++)
     {
-      if (checkColumn(i))
+      // lines
+      if (checkLine(i))
         result[i] = 1;
       else
         result[i] = 0;
+      // columns
+      if (checkColumn(i))
+        result[i+SIZE] = 1;
+      else
+        result[i+SIZE] = 0;
+      // subgrid
+      if (checkSubgrid(i))
+        result[i+(SIZE*2)] = 1;
+      else
+        result[i+(SIZE*2)] = 0;
     }
     fclose(file);
   }
   else
   {
 
-    fprintf(stderr, "%s not found\n", argv[1]);
+    fprintf(stderr, "File %s not found!\n", argv[1]);
     return -1;
   }
 
   int i;
-  for (i = 0; i < SIZE; i++)
+  for (i = 0; i < SIZE*2; i++)
   {
     if (!result[i]){
-      printf("Invalid solution\n");
+      printf("Invalid solution!\n");
       return 0;
     }
   }
-  printf("Valid solution\n");
+  printf("Valid solution!\n");
   return 0;
+}
+
+int checkLine(int lineNum)
+{
+  //size of line is 19
+  int i, line[SIZE], position = (lineNum * 19);
+  fseek(file, position, SEEK_SET);
+  for (i = 0; i < SIZE; i++)
+    fscanf(file, "%d", &line[i]);
+  return checkSudoku(line);
 }
 
 int checkColumn(int columnNum)
@@ -52,11 +75,31 @@ int checkColumn(int columnNum)
   int column[SIZE], i;
   for (i = 0; i < SIZE; i++)
   {
+    //columnNum * 2 because of empty spaces
     int position = (i * 19) + (columnNum * 2);
     fseek(file, position, SEEK_SET);
     fscanf(file, "%d", &column[i]);
   }
   return checkSudoku(column);
+}
+
+int checkSubgrid(int subgridNum)
+{
+  int i, subgrid[SIZE], position = (subgridNum % 3) * 6;;
+  if (subgridNum >= 3 && subgridNum < 6)
+    position = (3 * 19) + (subgridNum % 3) * 6;
+  else if (subgridNum >= 6)
+    position = (6 * 19) + (subgridNum % 3) * 6;
+  for (i = 0; i < SIZE; i++)
+  {
+    if (i == 3)
+      position += 13;
+    if (i == 6)
+      position += 13;
+    fseek(file, position + (i * 2), SEEK_SET);
+    fscanf(file, "%d", &subgrid[i]);
+  }
+  return checkSudoku(subgrid);
 }
 
 int checkSudoku(int array[SIZE])
@@ -71,33 +114,3 @@ int checkSudoku(int array[SIZE])
   }
   return 1;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//
